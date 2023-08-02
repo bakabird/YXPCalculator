2d3b731cd8a0d756edc15fce1e43e741ca61bafb
bakabird <bakabird@sina.cn>
2023-08-02 21:56:42 +0800

    混淆代码优化

--- src/Hunxiao/hunxiao.ts
+++ src/Hunxiao/hunxiao.ts
@@ -1,74 +1,70 @@
-// 检索的相对文件夹
 const relativePath = '..'
-// 不需要混淆的js或文件夹
 const exitFile = [
-    'node_modules', 'Hunxiao', "Lib", "Cfgs", "BrowerLibs", "out", ".git", 
+    'node_modules', 'Hunxiao', "Lib", "Cfgs", 
+    "BrowerLibs", "out", ".git",  "src", "img", "img1", "less", "css", "sprite", "tmp", "ui",
     'geneCardPinyin.js', 'gulpfile.js', "jquery.js"]
 
-
-var fs = require('fs')
+const fs = require('fs');
 import process from 'child_process'
-var readDir = fs.readdirSync(relativePath);
+const readDir = fs.readdirSync(relativePath);
+const czFile = [];
+const filePath = relativePath;
+const arr = [];
+const concurrency = 5; // 控制同时执行的命令数量
 
+readFile(readDir, filePath);
 
-// 需要存在的js
-const czFile = []
-
-var filePath = relativePath
-var arr = new Array()
-readFile(readDir, filePath)
-// 读取相对路径下的所有文件 
-function readFile(readDir, filePath) {
+async function readFile(readDir, filePath) {
     if (readDir.length > 0) {
-        for (var i = 0; i < readDir.length; i++) {
-            scannerFile(readDir[i], filePath)
+        for (let i = 0; i < readDir.length; i++) {
+            await scannerFile(readDir[i], filePath);
         }
     }
+    await executeObfuscator(arr);
+    console.log("over");
 }
-// 扫描文件进行检索出js文件进行混淆
-function scannerFile(file, filePath) { 
-    console.log("file-----" + file);
-    var readdirpath = ""
-    if (filePath == './') {
-        readdirpath = filePath + file 
-    } else {
-        readdirpath = filePath + "/" + file 
-    }
-    if (exitFile.indexOf(file) < 0) {
-        console.log('-->Start entering FS');
-        fs.stat(readdirpath, (err, data) => {
-            if (err) {
-                console.log(err);
-            } else {
-                if (data.isDirectory()) {
-                    console.log('-->isDirectory:' + file);
-                    var readChildDir = fs.readdirSync(readdirpath);
-                    console.log(readChildDir);
-                    readFile(readChildDir, readdirpath)
-                } else {
-                    console.log('-->isNotDirectory:' + file);
-                    if (file.indexOf('.js') >= 0 && file.indexOf('.json') < 0) {
-                        // 开始混淆代码 
-                        console.log('-->Start confusing code:' + file);
-                        var cmd = ' javascript-obfuscator ' + readdirpath + ' --config hunxiao.json --output ' + readdirpath;
-                        process.exec(cmd, function (error, stdout, stderr) {
-                            if(error) {
-                                console.log("error:" + error);
-                            }
-                            if(stderr != "") {
-                                console.log("stderr:" + stderr);
-                            }
-                            // console.log("stdout:" + stdout);
-                        });
-                        arr.push(readdirpath)
-                    } else {
-                        console.log('Non-folder - Non-js code :' + file);
-                    }
-                }
-            }
-        })
 
+async function scannerFile(file, filePath) {
+    if (exitFile.indexOf(file) < 0) {
+        const readdirpath = filePath === './' ? filePath + file : filePath + "/" + file;
+        const data = await fs.promises.stat(readdirpath);
+        if (data.isDirectory()) {
+            const readChildDir = fs.readdirSync(readdirpath);
+            readFile(readChildDir, readdirpath);
+        } else {
+            if (file.indexOf('.js') >= 0 && file.indexOf('.json') < 0) {
+                arr.push(readdirpath);
+                if (arr.length >= concurrency) {
+                    await executeObfuscator(arr.splice(0, concurrency));
+                }
+            } else {
+                console.log('Non-folder - Non-js code :' + file);
+            }
+        }
     } else {
         console.log('-->skip------------');
     }
+}
+
+function executeObfuscator(filesToObfuscate) {
+    console.log('Executing obfuscator for files:');
+    console.log(filesToObfuscate);
+    const obfuscationPromises = filesToObfuscate.map((file) => {
+        console.log('-->Start confusing code:' + file);
+        const cmd = 'javascript-obfuscator ' + file + ' --config hunxiao.json --output ' + file;
+        return new Promise<void>((resolve, reject) => {
+            process.exec(cmd, function (error, stdout, stderr) {
+                if (error) {
+                    console.log("error:" + error);
+                    reject(error);
+                }
+                if (stderr !== "") {
+                    console.log("stderr:" + stderr);
+                }
+                console.log("stdout:" + stdout);
+                resolve();
+            });
+        });
+    });
+    return Promise.all(obfuscationPromises);
 }
